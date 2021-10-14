2a838d2b818740d7132159c0adee2fb47f72096a
nayoung <evelyn82ny@gmail.com>
2021-10-14 19:59:25 +0900

    Change the file name

--- hello/hello_client.c
+++ hello/hello_client.c
@@ -14,7 +14,7 @@ int main(int argc, char* argv[])
 	char message[30];
 	int str_len;
 	
-	if(argc!=3){
+	if(argc != 3){
 		printf("Usage : %s <IP> <port>\n", argv[0]);
 		exit(1);
 	}
@@ -24,15 +24,15 @@ int main(int argc, char* argv[])
 		error_handling("socket() error");
 	
 	memset(&serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family=AF_INET;
-	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
-	serv_addr.sin_port=htons(atoi(argv[2]));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+	serv_addr.sin_port = htons(atoi(argv[2]));
 		
-	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1) 
+	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
 		error_handling("connect() error!");
 	
-	str_len=read(sock, message, sizeof(message)-1);
-	if(str_len==-1)
+	str_len=read(sock, message, sizeof(message) - 1);
+	if(str_len == -1)
 		error_handling("read() error!");
 	
 	printf("Message from server: %s \n", message);  
--- tcp/tcp_client.c
+++ tcp/tcp_client.c
@@ -13,10 +13,10 @@ int main(int argc, char* argv[])
 	int sock;
 	struct sockaddr_in serv_addr;
 	char message[30];
-	int str_len=0;
-	int idx=0, read_len=0;
+	int str_len = 0;
+	int idx = 0, read_len = 0;
 	
-	if(argc!=3){
+	if(argc != 3){
 		printf("Usage : %s <IP> <port>\n", argv[0]);
 		exit(1);
 	}
@@ -30,13 +30,11 @@ int main(int argc, char* argv[])
 	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
 	serv_addr.sin_port=htons(atoi(argv[2]));
 		
-	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1) 
+	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
 		error_handling("connect() error!");
 
-	while(read_len=read(sock, &message[idx++], 1))
-	{
-		if(read_len==-1)
-		{
+	while(read_len = read(sock, &message[idx++], 1)) {
+		if(read_len == -1){
 			error_handling("read() error!");
 			break;
 		}
