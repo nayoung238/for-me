267f10f40fe8800fce1480e8776ab8d4dd43564d
nayoung <evelyn82ny@gmail.com>
2021-12-03 11:20:00 +0900

    Upload code for zombie check

---
+++ code/signal/zombie_wait.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h> // fork
+#include <stdlib.h> // exit
+#include <sys/wait.h>
+
+int main(){
+    
+    pid_t pid = fork();
+    int state;
+    
+    if(pid == 0) {
+        puts("Hi! im child1");
+        return 3;
+    }
+    else {
+        printf("child1 id : %d\n", pid);
+        
+        pid = fork();
+        if(pid == 0) {
+            exit(7);
+        }
+        else {
+            puts("Hi! im child2");
+            printf("child2 id : %d\n", pid);
+            
+            wait(&state);
+            if(WIFEXITED(state))
+                printf("child return value : %d\n", WEXITSTATUS(state));
+            
+            wait(&state);
+            if(WIFEXITED(state))
+                printf("child return value : %d\n", WEXITSTATUS(state));
+        }
+    }
+    return 0;
+}
---
+++ code/signal/zombie_waitpid.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h> // fork
+#include <stdlib.h> // exit
+#include <sys/wait.h>
+
+int main(){
+    
+    pid_t pid = fork();
+    int state;
+    
+    if(pid == 0) {
+        puts("Hi! im child");
+        sleep(8);
+        return 3;
+    }
+    else {
+        while(!waitpid(-1, &state, WNOHANG)) {
+            sleep(3);
+            puts("sleep 3sec");
+        }
+        if(WIFEXITED(state))
+            printf("child return value : %d\n", WEXITSTATUS(state));
+    }
+    return 0;
+}
