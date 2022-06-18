746b9ad6b4648b47fdc6695ce555e6d9b2fa970c
nayoung <evelyn82ny@gmail.com>
2022-06-18 16:38:45 +0900

    Refactor: Code cleanup

--- Network/code/pipe/pipe_echo_client.c
+++ Network/code/pipe/pipe_echo_client.c
@@ -15,25 +15,25 @@ int main(int argc, char *argv[]) {
     struct sockaddr_in serv_adr;
 
     if(argc != 3) {
-	    printf("Usage : %s <IP> <port>\n", argv[0]);
-	    exit(1);
-	}
+        printf("Usage : %s <IP> <port>\n", argv[0]);
+        exit(1);
+    }
 
-	sock = socket(PF_INET, SOCK_STREAM, 0);
-	if(sock == -1)
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    if(sock == -1)
         error_handling("socket() error");
 
-	memset(&serv_adr, 0, sizeof(serv_adr));
-	serv_adr.sin_family = AF_INET;
-	serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
-	serv_adr.sin_port = htons(atoi(argv[2]));
+    memset(&serv_adr, 0, sizeof(serv_adr));
+    serv_adr.sin_family = AF_INET;
+    serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_adr.sin_port = htons(atoi(argv[2]));
 
-	if(connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1)
+    if(connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1)
         error_handling("connect() error!");
-	else
+    else
         puts("Connected...........");
 
-	while(1) {
+    while(1) {
         fputs("Input message(Q to quit): ", stdout);
         fgets(buf, BUF_SIZE, stdin);
 
@@ -44,9 +44,9 @@ int main(int argc, char *argv[]) {
         str_len = read(sock, buf, BUF_SIZE-1);
         buf[str_len] = 0;
         printf("Message from server: %s", buf);
-	}
-	close(sock);
-	return 0;
+    }
+    close(sock);
+    return 0;
 }
 
 void error_handling(const char *message) {
--- Network/code/pipe/single_pipe_using_sleep.c
+++ Network/code/pipe/single_pipe_using_sleep.c
@@ -7,25 +7,25 @@
 #define BUF_SIZE 1024
 
 int main(){
-	int fds[2];
-	char str_child[] = "I'm a child\n";
+    int fds[2];
+    char str_child[] = "I'm a child\n";
     char str_parent[] = "I'm a parent\n";
-	char buf[BUF_SIZE];
+    char buf[BUF_SIZE];
 
-	pipe(fds);
-	pid_t pid = fork();
+    pipe(fds);
+    pid_t pid = fork();
 
-	if(pid == 0) {
+    if(pid == 0) {
         write(fds[1], str_child, sizeof(str_child));
         sleep(2);
         read(fds[0], buf, BUF_SIZE);
         printf("Child proc output: %s\n", buf);
     }
-	else {
-		read(fds[0], buf, BUF_SIZE);
+    else {
+        read(fds[0], buf, BUF_SIZE);
         printf("Parent proc output : %s\n", buf);
         write(fds[1], str_parent, sizeof(str_parent));
         sleep(3);
-	}
-	return 0;
+    }
+    return 0;
 }
